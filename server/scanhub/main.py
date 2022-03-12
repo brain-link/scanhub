ba6da903699154f912659dc7026bf693c16319a1
David Schote <david.schote@web.de>
2022-03-12 16:10:59 +0100

    Docker container with server and postgres database

--- server/scanhub/main.py
+++ server/scanhub/main.py
@@ -1,48 +1,18 @@
 import graphene
+import uvicorn
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from tortoise.contrib.fastapi import register_tortoise
 
 from scanhub.graphene.query import Query
 from scanhub.graphene.subscription import Subscription
 from scanhub.graphene.mutation import Mutation
 
 
+app = FastAPI(
+    title="ScanHub"
+)
 
-from tortoise import Tortoise
-
-
-
-async def connectToDatabase():
-    await Tortoise.init(
-        db_url='postgres://brainLink:brainLinkIstCool2022UndLecker@postgres/scanhub',
-        modules={"models": ["scanhub.database.models"]}
-    )
-
-    # register_tortoise(
-    #     admin_app,
-    #     config={
-    #         "connections": {"default": settings.DATABASE_URL},
-    #         "apps": {
-    #             "models": {
-    #                 "models": ["scanhub.models"],
-    #                 "default_connection": "default",
-    #             }
-    #         },
-    #     },
-    #     generate_schemas=True,
-    # )
-
-    # register_tortoise(
-    #     admin_app,
-    #     db_url="postgres://brainLink:brainLinkIstCool2022UndLecker@postgres/scanhub",
-    #     modules={"models": ["scanhub.models"]},
-    #     generate_schemas=True,
-    #     add_exception_handlers=True,
-    # )
-
-
-
-app = FastAPI()
 schema = graphene.Schema(
     query=Query,
     mutation=Mutation,
@@ -52,7 +22,11 @@ schema = graphene.Schema(
 # Graphiql IDE
 app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))
 
-connectToDatabase()
-Tortoise.generate_schemas()
-# await connectToDatabase()
-# await Tortoise.generate_schemas()
+# Tortoise ORM    
+register_tortoise(
+    app,
+    db_url='postgres://brainLink:brainLinkIstCool2022UndLecker@postgres/scanhub',
+    modules={"models": ["scanhub.database.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
