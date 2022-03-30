376ad74f37c69fc76b448a0d2301746b13d45ae1
David Schote <david.schote@web.de>
2022-03-30 22:47:42 +0200

    Added graphql types

--- server/scanhub/graphene/types.py
+++ server/scanhub/graphene/types.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+from unicodedata import name
+
+from tomlkit import comment, date
 import graphene
 
 from scanhub.database.enums import PatientSex, PatientStatus
@@ -20,3 +24,38 @@ class Patient(graphene.ObjectType):
     concern = graphene.String(required=True)
     admission_date = graphene.DateTime(required=True)
     status = graphene.Field(PatientStatusGQL, required=True)
+
+
+class Device(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    modality = graphene.String(required=True)
+    address = graphene.String(required=True)
+    created_at = graphene.DateTime(required=True)
+    site_id = graphene.ID(required=True)        # is this an ID or: graphene.Field(Site, required=True) ?
+
+
+class Site(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    name = graphene.String(required=True)
+    city = graphene.String(required=True)
+    country = graphene.String(required=True)
+    address = graphene.String(required=True)
+
+
+class Procedure(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    date = graphene.DateTime(required=True)
+    reason = graphene.String(required=True)
+    patient_id = graphene.ID(required=True)     # is this an ID or: graphene.Field(Patient, required=True) ?
+
+
+class Recording(graphene.ObjectType):
+    id = graphene.ID(required=True)
+    date = graphene.DateTime(required=True)
+    thumbnail = graphene.ID(required=False)     # ID to blob storage ?
+    comment = graphene.String(required=True)
+    data = graphene.ID(required=False)          # ID to blob storage ?
+    device_id = graphene.ID(required=True)      # is this an ID or: graphene.Field(Device, required=True) ?
+    procedure_id = graphene.ID(required=True)   # is this an ID or: graphene.Field(Procedure, required=True) ?
+
+
