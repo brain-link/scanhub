cf853c001899caf6d70813eeaade0f46208c7b8f
David Schote <david.schote@web.de>
2022-06-02 00:05:11 +0200

    Integration of coreui-react

--- frontend/apps/test-client/src/App.tsx
+++ frontend/apps/test-client/src/App.tsx
@@ -1,12 +1,14 @@
-import 'scanhub-ui/css/index.css'
-import { RelayEnvironmentProvider } from 'react-relay'
-import { ThemeProvider } from 'styled-components'
+// import 'scanhub-ui/css/index.css'
+import './style.scss'
 
-import { environment } from './env'
-import { RouteConfig } from './components/Routes'
-import { DialogSystem, ErrorBoundary, theme } from 'scanhub-ui'
+import { RelayEnvironmentProvider } from 'react-relay'
 import { HashRouter } from 'react-router-dom'
 import { Suspense } from 'react'
+import { environment } from './env'
+import { RouteConfig } from './components/Routes'
+
+import { ThemeProvider } from 'styled-components'
+import { DialogSystem, ErrorBoundary, theme } from 'scanhub-ui'
 
 export function App() {
   return (
