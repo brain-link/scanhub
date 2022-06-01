af81f8418dcc22cb517949e45cf4777b81e078ce
David Schote <david.schote@web.de>
2022-06-02 00:07:04 +0200

    Coreui record cards

--- frontend/apps/test-client/src/components/Procedure.tsx
+++ frontend/apps/test-client/src/components/Procedure.tsx
@@ -2,6 +2,17 @@ import { Link, NavLink, Outlet, useParams } from 'react-router-dom'
 import { range } from 'utils'
 import { getModalityComponent } from './modalities'
 
+import {
+  CCard,
+  CCol,
+  CRow,
+  CCardBody,
+  CPlaceholder,
+  CCardImage,
+  CCardText,
+  CCardTitle
+} from '@coreui/react'
+
 async function startRecording() {
   await fetch(
     'http://localhost:81/api/TriggerAcquisition?cmd=MEAS_START',
@@ -21,15 +32,34 @@ export function ProcedureSidebar() {
           <span className='fa fa-filter' />
         </button>
       </div>
-      <div className='grow flex-col gap-2 pad-2 scroll-y'>
+      <div className='grow flex-col gap-2 pad-2 scroll-y m-4'>
         {range(50).map(i => (
           <Link key={i} to={`mri-recording-${i}`}>
             <section className='flex gap-2'>
-              <time dateTime='placeholder'>00:00</time>
+
+              <CCard className='mb-3' style={{ maxWidth: '500px' }}>
+                <CRow className="g-0">
+
+                  <CCol md={4}>
+                    <CCardImage src={`https://picsum.photos/64?i=${i}`} />
+                  </CCol>
+                  <CCol md={8}>
+                    <CCardBody>
+                      <CCardTitle>MRI Exam {i+1}</CCardTitle>
+                      <CCardText>This is some information about the MRI exam.</CCardText>
+                    </CCardBody>
+                  </CCol>
+                </CRow>
+              </CCard>
+
+              {/* <time dateTime='placeholder'>00:00</time>
               <img src={`https://picsum.photos/64?i=${i}`} loading='lazy' width={64} height={64} />
               <h1>MRI</h1>
-              <p>Some description</p>
+              <p>Some description</p> */}
             </section>
+
+
+
           </Link>
         ))}
       </div>
