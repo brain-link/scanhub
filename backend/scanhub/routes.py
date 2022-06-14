47b86a81b31cc694cbe41cb2511db13ddf8f6d1b
David Schote <david.schote@web.de>
2022-06-14 23:16:04 +0200

    Added routes

--- backend/scanhub/routes.py
+++ backend/scanhub/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from scanhub.models import Patient, Device
+from scanhub.models import Patient, Device, Procedures, Recordings, Site, User
 
 
 # Define an api router
@@ -11,6 +11,12 @@ async def root() -> dict:
     return dict(
         msg="Hello World!"
     )
+
+# Device table data
+@api_router.get("/devices/")
+async def get_devices() -> dict:
+    devices = await Device.all()
+    return devices
 
 # Patient table data
 @api_router.get("/patients/")
@@ -35,10 +41,20 @@ async def get_patient(patient_id: int) -> dict:
     #     status=patient.status
     # ) for patient in patients]
 
-# Device table data
-@api_router.get("/devices/")
-async def get_devices() -> dict:
-    devices = await Device.all()
-    return devices
+@api_router.get("/patients/{patient_id}/procedures/")
+async def get_procedures(patient_id: int) -> dict:
+    procedures = await Procedures.filter(patient_id=patient_id)
+    return procedures
 
+@api_router.get("/patients/{patient_id}/{procedure_id}/recordings/")
+async def get_recordings(patient_id: int, procedure_id: int) -> dict:
 
+    patient = await Patient.get(id=patient_id)
+    procedure = await Procedures.get(id=procedure_id)
+    recordings = await Recordings.filter(procedure_id=procedure_id)
+
+    return dict(
+        patient=patient,
+        procedure=procedure,
+        recordings=recordings
+    )
