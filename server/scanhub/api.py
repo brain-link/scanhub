9159289a020b94e932c74acdff7917eca57f7215
Johannes Behrens <behrens@neoscan-solutions.com>
2022-06-14 22:09:32 +0200

    Add simple fastapi websocket example.

--- server/scanhub/api.py
+++ server/scanhub/api.py
@@ -1,11 +1,32 @@
 # import uvicorn
-from fastapi import FastAPI, APIRouter, Request, Path
+from fastapi import FastAPI, APIRouter, Request, Path, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from tortoise.contrib.fastapi import register_tortoise
 from scanhub.database.models import Patient
 from scanhub.database.models import Device
 
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+            
+manager = ConnectionManager()
 
 app = FastAPI(
     title="ScanHub"
@@ -66,6 +87,19 @@ async def get_devices() -> dict:
 async def get_patient(patient_id: int) -> dict:
     patient = await Patient.get(id=patient_id)
     return patient
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
 app.include_router(api_router)
 
