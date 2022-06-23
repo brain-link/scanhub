77d7ae25ef3065e4c285885a8ce10160959b2cc0
David Schote <david.schote@web.de>
2022-06-23 18:52:41 +0200

    Dashboard placeholder

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,9 +1,20 @@
+import {
+    CCard,
+    CCardHeader,
+    CCardBody,
+    CCardTitle
+} from "@coreui/react"
+
 
 export function Dashboard() {
     return (
         <>
-            <h1>Dashboard</h1>
-            {/* <h2>{process.env.REACT_APP_SERVER}</h2>  */}
+            <CCard className='m-4'>
+                <CCardHeader className="h5">Dashboard</CCardHeader>
+                <CCardBody className='m-2 text-center'>
+                    <img src="https://brain-link.de/wp-content/uploads/2022/03/ScanHub.svg" width="400" height="200" alt="ScanHub"/>
+                </CCardBody>
+            </CCard>
         </>
     );
 }
