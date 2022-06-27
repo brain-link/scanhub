551597b46fc51b47a7828cb9ebff874008fe0bb7
David Schote <david.schote@web.de>
2022-06-27 18:16:44 +0200

    Implementation of sequence plot backend

--- backend/scanhub/routes.py
+++ backend/scanhub/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from scanhub.models import Patient, Device, Procedures, Recordings, Site, User
+from scanhub.utilities.sequence_plot import plot
 
 
 # Define an api router
@@ -39,36 +40,13 @@ async def get_patient(patient_id: int) -> dict:
     patient = await Patient.get(id=patient_id)
     return patient
 
-# TODO: Should we do it like this or just return whats in the database?
-# -> Interface mapping could be performed on the frontend side
-
-    # return [dict(
-    #     id=patient.id,
-    #     sex=patient.sex,
-    #     birthday=patient.birthday,
-    #     concern=patient.concern,
-    #     status=patient.status
-    # ) for patient in patients]
-
 @api_router.get("/patients/{patient_id}/procedures/")
 async def get_procedures(patient_id: int) -> dict:
     procedures = await Procedures.filter(patient_id=patient_id)
     return procedures
 
 @api_router.get("/patients/{patient_id}/{procedure_id}/records/")
-# async def get_recordings(patient_id: int, procedure_id: int) -> dict:
 async def get_recordings(procedure_id: int) -> dict:
-
-    # patient = await Patient.get(id=patient_id)
-    # procedure = await Procedures.get(id=procedure_id)
-    # recordings = await Recordings.filter(procedure_id=procedure_id)
-
-    # return dict(
-    #     patient=patient,
-    #     procedure=procedure,
-    #     recordings=recordings
-    # )
-
     records = await Recordings.filter(procedure_id=procedure_id)
     return records
 
@@ -79,7 +57,10 @@ async def get_record(record_id: int) -> dict:
 
 @api_router.post("/patients/{patient_id}/{procedure_id}/records/{record_id}/sequence/")
 async def set_sequence(parameter: list) -> dict:
-    # print("Hello World!")
     for param in parameter:
         print(param)
-    return dict()
+    return parameter
+
+@api_router.get("/test_sequence/")
+async def get_sequence() -> list:
+    return plot
