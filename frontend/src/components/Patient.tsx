900374fa8ad20f76e00ade7e763aad5e2bf71b23
David Schote <david.schote@web.de>
2022-07-10 23:38:10 +0200

    Add and delete procedures

--- frontend/src/components/Patient.tsx
+++ frontend/src/components/Patient.tsx
@@ -7,10 +7,23 @@ import {
   CRow, 
   CCard, 
   CCardHeader, 
+  CModalBody,
+  CModal,
+  CModalTitle,
+  CButton,
+  CModalHeader,
+  CForm,
+  CLink,
+  CFormInput,
+  CModalFooter,
+  CCloseButton, 
   CCardBody} from '@coreui/react';
 
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import { Procedure, Patient } from './Interfaces';
+import { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
 
 
 export function PatientIndex() {
@@ -73,44 +86,110 @@ export function PatientIndex() {
           <CCard>
             <CCardHeader className="h5">Procedures</CCardHeader>
             <CCardBody>
+              <div className="mb-2 d-flex w-100 justify-content-start">
+                <NewProcedure />
+              </div>
               <CListGroup>
                 {
                   procedures?.map(procedure => (
-                    <CListGroupItem component='a' href={`${patient.id}/${procedure.id}`}>
+                    <CListGroupItem component={CLink} to={`${patient.id}/${procedure.id}`}>
                       <div className="d-flex w-100 justify-content-between">
                         <h5 className="mb-1">Procedure {procedure.id}</h5>
-                        <small>{procedure.date}</small>
+                        <DeleteWarning procedure_id={procedure.id} />
                       </div>
+                      <small>{procedure.date}</small>
                       <p className="mb-1">
                         Description of the procedure, that contains information about the modalities, number of records, etc.
                       </p>
-                      {/* <small>Donec id elit non mi porta.</small> */}
                     </CListGroupItem>
                   ))
                 }
               </CListGroup>
-
-              {/* <CRow className='m-2'>
-                <CCol>
-                  {
-                    procedures?.map(procedure => (
-                      <CWidgetStatsF
-                        // className="mb-3"
-                        color="primary"
-                        title={procedure.date}
-                        value={
-                          <CLink to='test-procedure' component={ Link }> Procedure {procedure.id} </CLink>
-                        }
-                      />
-                    ))
-                  }
-                </CCol>
-              </CRow> */}
-
             </CCardBody>
           </CCard>
         </CCol>
       </CRow>
+    </>
+  )
+}
+
+function DeleteWarning({procedure_id}) {
+  let params = useParams()
+  const [visible, setVisible] = useState(false)
+  const deletePost = async () => {
+    return await axios.delete(`http://localhost:8000/patients/${params.patientId}/${procedure_id}/`);
+  }
+  return (
+    <>
+      <CCloseButton onClick={ () => setVisible(!visible) }/>
+      <CModal visible={visible} onClose={() => setVisible(false)}>
+        <CModalHeader>
+          <CModalTitle>Delete Record</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <p>
+            You are about to delete procedure { procedure_id }, are you sure that you want to proceed?
+          </p>
+          <CButton
+            color='danger'
+            variant='outline'
+            onClick={ () => {deletePost(); setVisible(false)}  }
+          > Confirm Delete </CButton>
+        </CModalBody>
+
+      </CModal>
+    </>
+  )
+}
+
+
+function NewProcedure() {
+
+  let params = useParams()
+  let patient = parseInt(params.patientId ? params.patientId : "0");
+  const [data, setData] = useState<Procedure>({ id: 0, patient_id: patient, reason: "", date: "" })
+  const [visible, setVisible] = useState(false)
+
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    })
+  }
+  
+  const mutation = useMutation(async() => {
+    return await axios.post(`http://localhost:8000/patients/${params.patientId}/procedures/new/`, data)
+    .catch( (error) => { console.log(error) });
+  })
+
+  return (
+    <>
+      <CButton 
+        color="primary" 
+        onClick={() => setVisible(!visible)}
+        variant="outline"
+      >
+        New Procedure
+      </CButton>
+      <CModal visible={visible} onClose={() => setVisible(false)}>
+        <CModalHeader>
+          <CModalTitle>Create New Record</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CForm>
+            <CFormInput
+              id="floatingInputValue"
+              name="reason"
+              label="Reason" 
+              placeholder={ data.reason }
+              text="Please indicate the reason for the procedure."
+              onChange={ handleChange }/>
+          </CForm>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="primary" onClick={ () => { mutation.mutate(); setVisible(false) }}>Save</CButton>
+        </CModalFooter>
+      </CModal>
     </>
   )
 }
