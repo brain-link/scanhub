c4df7c95c4150dbfb33ae2f82fde060473345207
David Schote <david.schote@web.de>
2022-09-28 22:39:41 +0200

    Revision of dashboard with mui

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,31 +1,30 @@
-import {
-    CCard,
-    CCardHeader,
-    CCardBody,
-    CNavbarBrand
-} from "@coreui/react"
+import React from "react";
 
+import {
+    Card,
+    CardContent,
+    IconButton
+} from "@mui/material"
 
 export function Dashboard() {
     return (
         <>
-            <CCard className='m-4'>
-                <CCardHeader className="h5">Dashboard</CCardHeader>
-                <CCardBody className='m-2 text-center'>
+            <Card className='m-4'>
+                <CardContent className='m-2 text-center'>
                     <img src="https://brain-link.de/wp-content/uploads/2022/03/ScanHub.svg" width="400" height="200" alt="ScanHub"/>
                     <div className="mt-5">
                         <span><small>ScanHub &copy; 2022, Powered by BRAIN-LINK</small></span>
-                        <CNavbarBrand href="https://www.brain-link.de/">
+                        <IconButton href="https://www.brain-link.de/">
                             <img
                                 src='https://avatars.githubusercontent.com/u/27105562?s=200&v=4'
                                 alt=""
                                 height="30"
-                                className="d-inline-block ms-2"
+                                className="d-inline-block"
                             />
-                        </CNavbarBrand>
+                        </IconButton>
                     </div>
-                </CCardBody>
-            </CCard>
+                </CardContent>
+            </Card>
         </>
     );
 }
