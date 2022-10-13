059a47718597aafb30beb5a93cb4ddf823fc2c5a
David Schote <david.schote@web.de>
2022-10-13 19:59:07 +0200

    Mui dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 
+// import Card from '@mui/joy/Card';
+
 import {
     Card,
-    CardContent,
-    IconButton
+    Container,
+    IconButton,
+    Box,
+    CardMedia,
+    Typography
 } from "@mui/material"
 
 export function Dashboard() {
     return (
-        <>
-            <Card className='m-4'>
-                <CardContent className='m-2 text-center'>
-                    <img src="https://brain-link.de/wp-content/uploads/2022/03/ScanHub.svg" width="400" height="200" alt="ScanHub"/>
-                    <div className="mt-5">
-                        <span><small>ScanHub &copy; 2022, Powered by BRAIN-LINK</small></span>
-                        <IconButton href="https://www.brain-link.de/">
-                            <img
-                                src='https://avatars.githubusercontent.com/u/27105562?s=200&v=4'
-                                alt=""
-                                height="30"
-                                className="d-inline-block"
-                            />
-                        </IconButton>
-                    </div>
-                </CardContent>
-            </Card>
-        </>
+        <Container maxWidth={false} sx={{ display: 'flex', justifyContent: 'center'}}>
+            <Card sx={{ maxWidth: 600, p: 10}}>
+                <CardMedia
+                    component="img"
+                    width="200"
+                    src="https://brain-link.de/wp-content/uploads/2022/03/ScanHub.svg"
+                    sx={{ m: 2 }}
+                />
+
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 5, gap: 1}}>
+
+                    <Typography>ScanHub &copy; 2022, Powered by BRAIN-LINK</Typography>
+                    <IconButton href="https://www.brain-link.de/">
+                        <img
+                            src='https://avatars.githubusercontent.com/u/27105562?s=200&v=4'
+                            alt=""
+                            height="30"
+                            className="d-inline-block"
+                        />
+                    </IconButton>
+
+                </Box>
+                        
+            </Card>       
+        </Container>
+             
     );
 }
