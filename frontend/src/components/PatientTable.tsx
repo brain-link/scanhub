1f4fc0b0b0915b1eab05a3a984f357601ab5d2e1
David Schote <david.schote@web.de>
2022-10-17 20:08:42 +0200

    Patient table

--- frontend/src/components/PatientTable.tsx
+++ frontend/src/components/PatientTable.tsx
@@ -1,51 +1,74 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-import { useQuery } from "react-query";
+import { Link as RouterLink } from 'react-router-dom';
 import { Patient } from './Interfaces'
-
-// import {
-//     CCard,
-//     CCardBody,
-//     CContainer,
-//     CSpinner,
-//     CCardSubtitle,
-//     CCardTitle,
-//     CTable,
-//     CTableBody,
-//     CTableDataCell,
-//     CTableHead,
-//     CTableRow,
-//     CTableHeaderCell,
-//     CBadge,
-//     CNavLink,
-//     CCardHeader,
-// } from '@coreui/react'
-
-import Container from '@mui/material/Container';
+import { useQuery } from "react-query";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from "@mui/material/TableHead";
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import LinearProgress from '@mui/material/LinearProgress';
-import Box from '@mui/material/Box'
-import { Typography } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/system/Container';
+import Box from '@mui/joy/Box'
 
 export function PatientTable() {
 
     // Syncing our data
     const { data: patients, isSuccess } = useQuery<Patient[]>("patients/");
 
-    // if (!isSuccess) {
-    //     return (
-    //         <Container maxWidth={false} sx={{ width: '50%', mt: 5, }}>
-    //             <Typography align='center'>Loading patients...</Typography>
-    //             <LinearProgress />
-    //         </Container>
-    // )
-
+    if (!isSuccess) {
+        return (
+            <Container maxWidth={false} sx={{ width: '50%', mt: 5, }}>
+                <Typography align='center'>Loading patients...</Typography>
+                <LinearProgress />
+            </Container>
+    )}
 
     return (
-        <Container maxWidth={false} sx={{ width: '50%', mt: 5, }}>
-            <Typography align='center'>Under construction...</Typography>
-            <LinearProgress />
-        </Container>
-    )
+        <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2, pt: 0 }}>
+            <Paper sx={{ overflow: 'scroll', maxHeight: '82vh'}}>
+                
+                <TableContainer>
+                    <Table stickyHeader aria-label="Device Table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><Typography variant="button">ID</Typography></TableCell>
+                                <TableCell><Typography variant="button">Sex</Typography></TableCell>
+                                <TableCell><Typography variant="button">Birthday</Typography></TableCell>
+                                <TableCell><Typography variant="button">Status</Typography></TableCell>
+                                <TableCell><Typography variant="button">Concern</Typography></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {patients?.map(patient => (
+                                <TableRow component={RouterLink} to="/test" key={patient.id} color='primary' >
+                                    <TableCell>{ patient.id }</TableCell>
+                                    <TableCell>{ patient.sex }</TableCell>
+                                    <TableCell>{ patient.birthday }</TableCell>
+                                    <TableCell>{ patient.status }</TableCell>
+                                    <TableCell>{ patient.concern }</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            {/* <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={devices.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            /> */}
+            </Paper>
+        </Box>
+    );
 
     // return (
     //     <CCard className='m-4'>
