97d297e6310ff68abc5e499d5fbb77a685470f47
David Schote <david.schote@web.de>
2022-10-17 21:29:14 +0200

    Cleanup device and patient table

--- frontend/src/components/DeviceTable.tsx
+++ frontend/src/components/DeviceTable.tsx
@@ -1,83 +1,56 @@
-// import { Link } from 'react-router-dom'
-
+import * as React from 'react';
 import { useQuery } from "react-query";
 import { Device } from './Interfaces'
-
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from "@mui/material/TableHead";
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import LinearProgress from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
+import Typography from '@mui/joy/Typography';
 import Container from '@mui/system/Container';
 import Box from '@mui/joy/Box'
 
 
-export function DeviceTable() {
+export default function DeviceTable() {
   
     const { data: devices, isSuccess } = useQuery<Device[]>("devices/");
 
     if (!isSuccess) {
         return (
-            <Container maxWidth={false} sx={{ width: '50%', mt: 5, }}>
-                <Typography align='center'>Loading devices...</Typography>
+            <Container maxWidth={false} sx={{ width: '50%', mt: 5, justifyContent: 'center' }}>
+                <Typography>Loading devices...</Typography>
                 <LinearProgress />
             </Container>
         )
     }
 
-    // const [page, setPage] = React.useState(0);
-    // const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-    // const handleChangePage = (event: unknown, newPage: number) => {
-    //     setPage(newPage);
-    // };
-
-    // const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setRowsPerPage(+event.target.value);
-    //     setPage(0);
-    // };
-      
     return (
-        <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2, pt: 0 }}>
-            <Paper sx={{ overflow: 'scroll', maxHeight: '82vh'}}>
-                
-                <TableContainer>
-                    <Table stickyHeader aria-label="Device Table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell><Typography variant="button">ID</Typography></TableCell>
-                                <TableCell><Typography variant="button">Modality</Typography></TableCell>
-                                <TableCell><Typography variant="button">Location</Typography></TableCell>
+        <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2 }}>
+            {/* <Paper sx={{ overflow: 'scroll', maxHeight: '82vh'}}> */}
+
+            <TableContainer component={Paper} sx={{ overflow: 'scroll', maxHeight: '90vh' }}>
+                <Table stickyHeader aria-label="Device Table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><Typography level="h5">ID</Typography></TableCell>
+                            <TableCell><Typography level="h5">Modality</Typography></TableCell>
+                            <TableCell><Typography level="h5">Location</Typography></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {devices?.map(device => (
+                            <TableRow hover={true} key={device.id}>
+                                <TableCell>{ device.id }</TableCell>
+                                <TableCell>{ device.modality }</TableCell>
+                                <TableCell>{ device.address }</TableCell>
                             </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {devices?.map(device => (
-                                <TableRow key={device.id}>
-                                    <TableCell>{ device.id }</TableCell>
-                                    <TableCell>{ device.modality }</TableCell>
-                                    <TableCell>{ device.address }</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            {/* <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={devices.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            /> */}
-            </Paper>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     );
 }
--- frontend/src/components/PatientTable.tsx
+++ frontend/src/components/PatientTable.tsx
@@ -7,106 +7,57 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from "@mui/material/TableHead";
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import LinearProgress from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
+import Typography from '@mui/joy/Typography';
 import Container from '@mui/system/Container';
 import Box from '@mui/joy/Box'
 
-export function PatientTable() {
+export default function PatientTable() {
 
     // Syncing our data
     const { data: patients, isSuccess } = useQuery<Patient[]>("patients/");
 
     if (!isSuccess) {
         return (
-            <Container maxWidth={false} sx={{ width: '50%', mt: 5, }}>
-                <Typography align='center'>Loading patients...</Typography>
+            <Container maxWidth={false} sx={{ width: '50%', mt: 5, justifyContent: 'center' }}>
+                <Typography>Loading patients...</Typography>
                 <LinearProgress />
             </Container>
     )}
 
     return (
-        <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2, pt: 0 }}>
-            <Paper sx={{ overflow: 'scroll', maxHeight: '82vh'}}>
-                
-                <TableContainer>
-                    <Table stickyHeader aria-label="Device Table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell><Typography variant="button">ID</Typography></TableCell>
-                                <TableCell><Typography variant="button">Sex</Typography></TableCell>
-                                <TableCell><Typography variant="button">Birthday</Typography></TableCell>
-                                <TableCell><Typography variant="button">Status</Typography></TableCell>
-                                <TableCell><Typography variant="button">Concern</Typography></TableCell>
+        // <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2, pt: 0 }}>
+        //     <Paper sx={{ overflow: 'scroll', maxHeight: '82vh'}}>
+        
+        <Box sx={{ flexGrow: 1, justifyContent: 'center', p: 2 }}> 
+            <TableContainer component={Paper} sx={{ overflow: 'scroll', maxHeight: '90vh' }}>
+                <Table stickyHeader aria-label="Device Table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><Typography level="h5">ID</Typography></TableCell>
+                            <TableCell><Typography level="h5">Sex</Typography></TableCell>
+                            <TableCell><Typography level="h5">Birthday</Typography></TableCell>
+                            <TableCell><Typography level="h5">Status</Typography></TableCell>
+                            <TableCell><Typography level="h5">Concern</Typography></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {patients?.map(patient => (
+                            <TableRow hover={true} component={RouterLink} to="/test" key={patient.id} sx={{ textDecoration: 'none' }}>
+                                <TableCell>{ patient.id }</TableCell>
+                                <TableCell>{ patient.sex }</TableCell>
+                                <TableCell>{ patient.birthday }</TableCell>
+                                <TableCell>{ patient.status }</TableCell>
+                                <TableCell>{ patient.concern }</TableCell>
                             </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {patients?.map(patient => (
-                                <TableRow component={RouterLink} to="/test" key={patient.id} color='primary' >
-                                    <TableCell>{ patient.id }</TableCell>
-                                    <TableCell>{ patient.sex }</TableCell>
-                                    <TableCell>{ patient.birthday }</TableCell>
-                                    <TableCell>{ patient.status }</TableCell>
-                                    <TableCell>{ patient.concern }</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            {/* <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={devices.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            /> */}
-            </Paper>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     );
-
-    // return (
-    //     <CCard className='m-4'>
-    //         <CCardHeader className="h5">Patients</CCardHeader>
-    //         <CCardBody className='m-2'>
-    //             {/* <CCardTitle className="mb-4">List of all Patients</CCardTitle> */}
-    //             <CTable hover>
-    //                 <CTableHead color='secondary'>
-    //                     <CTableRow>
-    //                         <CTableHeaderCell scope="col">ID</CTableHeaderCell>
-    //                         <CTableHeaderCell scope="col">Sex <span className='fa fa-arrow-down' /></CTableHeaderCell>
-    //                         <CTableHeaderCell scope="col">Birthday</CTableHeaderCell>
-    //                         <CTableHeaderCell scope="col">Status</CTableHeaderCell>
-    //                         <CTableHeaderCell scope="col">Concern</CTableHeaderCell>
-    //                         <CTableHeaderCell scope="col"><input type='checkbox' /></CTableHeaderCell>
-    //                     </CTableRow>
-    //                 </CTableHead>
-    //                 <CTableBody>
-    //                     {
-    //                         // !loading ? patients.map(patient => (    
-    //                         patients?.map(patient => (    
-    //                             <CTableRow align="middle" key={patient.id}>
-    //                                 <CTableHeaderCell scope="row">
-    //                                     <CNavLink to={`/patients/${patient.id}`} component={Link}>{patient.id}</CNavLink>
-    //                                 </CTableHeaderCell>
-    //                                 <CTableDataCell>{patient.sex}</CTableDataCell>
-    //                                 <CTableDataCell>{patient.birthday}</CTableDataCell>
-    //                                 <CTableDataCell>{patient.status}</CTableDataCell>
-    //                                 <CTableDataCell>{patient.concern}</CTableDataCell>
-    //                                 <CTableDataCell><input type='checkbox' /></CTableDataCell>
-    //                             </CTableRow>
-    //                         ))
-    //                     }
-    //                 </CTableBody>
-    //             </CTable>
-    //         </CCardBody>
-    //     </CCard>
-    // );
 }
 
 
