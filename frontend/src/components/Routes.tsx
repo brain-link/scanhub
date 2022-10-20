c3bc11571dcdac0408031b38b652e071428c46b7
David Schote <david.schote@web.de>
2022-10-20 23:30:25 +0200

    New routes and cleanup

--- frontend/src/components/Routes.tsx
+++ frontend/src/components/Routes.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { Suspense } from 'react'
 import React from 'react';
 
 import Dashboard from './Dashboard'
 import DeviceTable from "./DeviceTable";
 import PatientTable from "./PatientTable";
-import { App } from './App'
+import App from './App'
 import PatientIndex from './PatientPage'
-import { Procedure, ProcedureMainContentSwitcher, ProcedureMainContent } from "./Procedure";
+import Records from './Records';
+import PatientPageMainView from "./PatientPageMainView";
 
 export function RouteConfiguration () {
     return (
@@ -15,27 +15,18 @@ export function RouteConfiguration () {
             <Route path='/' element={<App />}>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/patients">
-                    <Route index element={
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <PatientTable />
-                        </Suspense>
-                    } />
-                    <Route path=':patientId'>
-                        <Route index element={<PatientIndex />} />
-                        <Route path=':procedureId' element={<Procedure />} >
-                            <Route path=':recordingId' element={<ProcedureMainContentSwitcher />}>
-                                <Route index element={<ProcedureMainContent />} />
-                                <Route path=':content' element={<ProcedureMainContent />} />
+                    <Route index element={<PatientTable />} />
+                    <Route path=':patientId' element={<PatientIndex/>} >
+                        <Route path=':procedureId' element={<Records/>}>
+                            <Route path=':recordId' element={<PatientPageMainView/>}>
+                                {/* <Route path='/view'/> */}
+                                <Route path=':toolId'/>
                             </Route>
                         </Route>
                     </Route>
                 </Route>
                 <Route path="/devices">
-                    <Route index element={
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <DeviceTable />
-                    </Suspense>
-                    } />
+                    <Route index element={ <DeviceTable /> } />
                 </Route> 
             </Route>
       </Routes>
