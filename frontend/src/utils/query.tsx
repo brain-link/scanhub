4fb5b878310a0383ca1de594ded4828eae76003f
David Schote <david.schote@web.de>
2022-10-20 23:30:05 +0200

    Global config in query

--- frontend/src/utils/query.tsx
+++ frontend/src/utils/query.tsx
@@ -1,8 +1,9 @@
-import axios from "axios";
-import { QueryFunction } from "react-query";
+import axios from 'axios';
+import { QueryFunction } from 'react-query';
+import config from './config';
 
 export const query: QueryFunction = async ({ queryKey }) => {
     // TODO: Use the environment variable SERVER here (process.env.SERVER)
-    const { data } = await axios.get(`http://localhost:8000/${queryKey[0]}`);
+    const { data } = await axios.get(`${config.baseURL}${queryKey[0]}`);
     return data;
 };
