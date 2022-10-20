67e27a8119f3b144d07d8b701275cf4a86c7fba1
David Schote <david.schote@web.de>
2022-10-20 23:26:57 +0200

    Add size constraints to theme

--- frontend/src/utils/theme.tsx
+++ frontend/src/utils/theme.tsx
@@ -1,5 +1,28 @@
+import * as React from 'react';
 import colors from '@mui/joy/colors';
-import { experimental_extendTheme as extendMuiTheme } from '@mui/material/styles';
+import { deepmerge } from '@mui/utils';
+import { createTheme, experimental_extendTheme as extendMuiTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+    interface Theme {
+        navigation: { height: string },
+        patientView: { 
+            toolbarHeight: string;
+            drawerWidth: string;
+            recordsWidth: string;
+        },
+    }
+
+    // allow configuration using `createTheme`
+    interface ThemeOptions {
+        navigation?: { height?: string },
+        patientView?: { 
+            toolbarHeight?: string;
+            drawerWidth?: string;
+            recordsWidth?: string;
+        },
+    }
+}
 
 const muiTheme = extendMuiTheme({
     // This is required to point to `var(--joy-*)` because we are using `CssVarsProvider` from Joy UI.
@@ -66,4 +89,15 @@ const muiTheme = extendMuiTheme({
     },
 });
 
-export default muiTheme;
+const customProps = createTheme(muiTheme, {
+    navigation: {
+        height: '64px',
+    },
+    patientView: {
+        toolbarHeight: '54px',
+        drawerWidth: '300px',
+        recordsWidth: '300px',
+    }
+});
+
+export default deepmerge(muiTheme, customProps);
