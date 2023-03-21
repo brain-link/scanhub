b8d1686ae84b70c4681bc05c5f9e3ccd88531e3e
David Schote <david.schote@web.de>
2023-03-21 08:58:32 +0100

    device orm model

--- services/device-manager/app/api/db.py
+++ services/device-manager/app/api/db.py
@@ -1,23 +1,40 @@
 import os
+import datetime
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
-from databases import Database
 
-DATABASE_URI = os.getenv('DATABASE_URI')
+# Create base for device
+Base = declarative_base()
+engine = create_engine(os.getenv('DB_URI'), echo=False)
 
-engine = create_engine(DATABASE_URI, echo=True)
 
-Base = automap_base()
+def init_db() -> None:
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
-# reflect the tables
-Base.prepare(autoload_with=engine)
 
-# get device table
-try:
-    device = Base.classes.device
-except ArithmeticError as err:
-    print("Table does not exist: ", err)
-    
-database = Database(DATABASE_URI)
+class Device(Base):
+    """
+    Device ORM model
+    """
+    __tablename__ = 'device'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(nullable=False)
+    manufacturer: Mapped[str] = mapped_column(nullable=False)
+    modality: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(nullable=False)
+    site: Mapped[str] = mapped_column(nullable=True)
+    ip_address: Mapped[str] = mapped_column(nullable=False)
+
+    datetime_created: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
+    datetime_updated: Mapped[datetime.datetime] = mapped_column(onupdate=func.now())
+
+
+# Create async engine and session, echo=True generates console output
+async_engine = create_async_engine(os.getenv('DB_URI_ASYNC'), future=True)  #, echo=True)
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
