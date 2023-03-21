6b742480012aaf9f2333f1feff24913a9c9d56b2
David Schote <david.schote@web.de>
2023-03-21 08:59:44 +0100

    exam endpoints

--- services/exam-manager/app/api/exam.py
+++ services/exam-manager/app/api/exam.py
@@ -15,14 +15,14 @@ exam = APIRouter()
 
 
 @exam.get('/exam/{id}', response_model=ExamOut, tags=["exam"])
-async def get_exam(id: str) -> Any:
+async def get_exam(id: int) -> Any:
     exam = await dal.get_exam(id)
     if not exam:
         raise HTTPException(status_code=404, detail="Exam not found")
     return exam
 
 @exam.get('/procedure/{id}', response_model=ProcedureOut)
-async def get_procedure(id: str) -> Any:
+async def get_procedure(id: int) -> Any:
     procedure = await dal.get_procedure(id)
     if not procedure:
         raise HTTPException(status_code=404, detail="Procedure not found")
@@ -36,15 +36,15 @@ async def get_procedure(id: str) -> Any:
 #     return record
 
 @exam.get('/exams/{patientID}', response_model=list[ExamOut], tags=["exam"])
-async def get_exams(patientID: str) -> Any:
-    exams = await dal.get_exams(patientID)
+async def get_exams(patient_id: str) -> Any:
+    exams = await dal.get_all_exams(patient_id)
     if not exams:
         raise HTTPException(status_code=404, detail="Exams not found")
     return exams
 
 
 @exam.get('/procedures/{examID}', response_model=list[ProcedureOut])
-async def get_procedures(examID: str) -> Any:
+async def get_procedures(examID: int) -> Any:
     procedures = await dal.get_procedures(examID)
     if not procedures:
         raise HTTPException(status_code=404, detail="Procedures not found")
@@ -63,6 +63,7 @@ async def create_exam(payload: BaseExam) -> Any:
 
 @exam.post('/procedure', response_model=ProcedureOut, status_code=201)
 async def create_procedure(payload: ProcedureIn) -> Any:
+    print(payload)
     return await dal.add_procedure(payload)
 
 # @exam.post('/record', response_model=RecordOut, status_code=201)
@@ -80,12 +81,12 @@ async def create_procedure(payload: ProcedureIn) -> Any:
 
 #     return response
 
-@exam.delete('/exam/{id}', response_model=ExamOut, tags=["exam"])
-async def delete_exam(id: int) -> Any:
-    response = await dal.delete_exam(id)
-    if not response:
-        raise HTTPException(status_code=404, detail="Exam not found")
-    return response
+# @exam.delete('/exam/{id}', response_model=ExamOut, tags=["exam"])
+# async def delete_exam(id: int) -> Any:
+#     response = await dal.delete_exam(id)
+#     if not response:
+#         raise HTTPException(status_code=404, detail="Exam not found")
+#     return response
 
 # @exam.delete('/procedure/{id}', response_model=ExamOut)
 # async def delete_procedure(id: int) -> Any:
@@ -101,9 +102,9 @@ async def delete_exam(id: int) -> Any:
 #         raise HTTPException(status_code=404, detail="Record not found")
 #     return record
 
-@exam.put('/exam/{id}', response_model=ExamOut, tags=["exam"])
-async def update_exam(id: int, payload: BaseExam) -> Any:
-    exam = await dal.update_exam(id, payload)
-    if not exam:
-        raise HTTPException(status_code=404, detail="Exam not found")
-    return exam
+# @exam.put('/exam/{id}', response_model=ExamOut, tags=["exam"])
+# async def update_exam(id: int, payload: BaseExam) -> Any:
+#     exam = await dal.update_exam(id, payload)
+#     if not exam:
+#         raise HTTPException(status_code=404, detail="Exam not found")
+#     return exam
