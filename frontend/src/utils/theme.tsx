839b1099459abbb5c2fa3667208e71086595d6d8
David Schote <david.schote@web.de>
2023-04-12 20:33:02 +0200

    Restructuring

--- frontend/src/utils/theme.tsx
+++ frontend/src/utils/theme.tsx
@@ -1,99 +1,12 @@
-// import { deepmerge } from '@mui/utils';
-// import {
-//   experimental_extendTheme as extendMuiTheme,
-// } from '@mui/material/styles';
-// import colors from '@mui/joy/colors';
-// import {
-//   extendTheme as extendJoyTheme,
-//   CssVarsProvider,
-// } from '@mui/joy/styles';
-
-import {
-    extendTheme as extendJoyTheme,
-    CssVarsProvider,
-  } from '@mui/joy/styles';
-
-
-import type {} from '@mui/material/themeCssVarsAugmentation';
+import { deepmerge } from '@mui/utils';
 import {
   experimental_extendTheme as extendMuiTheme,
-  PaletteColor,
-  TypeText,
-  TypeAction,
-  Overlays,
-  PaletteColorChannel,
-  PaletteAlert,
-  PaletteAppBar,
-  PaletteAvatar,
-  PaletteChip,
-  PaletteFilledInput,
-  PaletteLinearProgress,
-  PaletteSlider,
-  PaletteSkeleton,
-  PaletteSnackbarContent,
-  PaletteSpeedDialAction,
-  PaletteStepConnector,
-  PaletteStepContent,
-  PaletteSwitch,
-  PaletteTableCell,
-  PaletteTextChannel,
-  PaletteTooltip,
-  Shadows,
-  ZIndex,
 } from '@mui/material/styles';
-import { Theme as JoyTheme } from '@mui/joy/styles';
 import colors from '@mui/joy/colors';
-import { deepmerge } from '@mui/utils';
-
-// type JoyComponents = CssVarsThemeOptions['components'];
-
-// extends Joy theme to include tokens from Material UI
-declare module '@mui/joy/styles' {
-  interface Palette {
-    secondary: PaletteColorChannel;
-    error: PaletteColorChannel;
-    dividerChannel: string;
-    action: TypeAction;
-    Alert: PaletteAlert;
-    AppBar: PaletteAppBar;
-    Avatar: PaletteAvatar;
-    Chip: PaletteChip;
-    FilledInput: PaletteFilledInput;
-    LinearProgress: PaletteLinearProgress;
-    Skeleton: PaletteSkeleton;
-    Slider: PaletteSlider;
-    SnackbarContent: PaletteSnackbarContent;
-    SpeedDialAction: PaletteSpeedDialAction;
-    StepConnector: PaletteStepConnector;
-    StepContent: PaletteStepContent;
-    Switch: PaletteSwitch;
-    TableCell: PaletteTableCell;
-    Tooltip: PaletteTooltip;
-  }
-  interface PalettePrimary extends PaletteColor {}
-  interface PaletteInfo extends PaletteColor {}
-  interface PaletteSuccess extends PaletteColor {}
-  interface PaletteWarning extends PaletteColor {}
-//   interface PaletteCommon extends CommonColors {}
-  interface PaletteText extends TypeText {}
-//   interface PaletteBackground extends TypeBackground {}
-
-  interface ThemeVars {
-    // attach to Joy UI `theme.vars`
-    shadows: Shadows;
-    overlays: Overlays;
-    zIndex: ZIndex;
-  }
-}
-
-declare module '@mui/material/styles' {
-  interface Theme {
-    // put everything back to Material UI `theme.vars`
-    vars: JoyTheme['vars'];
-  }
-}
-
-
+import {
+  extendTheme as extendJoyTheme,
+  CssVarsProvider,
+} from '@mui/joy/styles';
 
 const { unstable_sxConfig: muiSxConfig, ...muiTheme } = extendMuiTheme({
   // This is required to point to `var(--joy-*)` because we are using
@@ -188,5 +101,6 @@ mergedTheme.unstable_sxConfig = {
   ...muiSxConfig,
   ...joySxConfig
 };
+
 
 export default mergedTheme;
