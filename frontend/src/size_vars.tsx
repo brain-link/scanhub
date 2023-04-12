2bfc56e4f01354cec0c50cfa4f1bf5d13cacc2e3
David Schote <david.schote@web.de>
2023-04-12 20:32:38 +0200

    Global size variables

---
+++ frontend/src/size_vars.tsx
@@ -0,0 +1,11 @@
+// Definition of global variables
+
+export const navigation = {
+    height: '64px',
+}
+
+export const patientView = {
+    toolbarHeight: '54px',
+    drawerWidth: '300px',
+    recordsWidth: '300px',
+}
