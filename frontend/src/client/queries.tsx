6f715c104f4cea85285958c21e867fdcf2ed00ff
David Schote <david.schote@web.de>
2023-04-28 20:21:19 +0200

    Revision of query client

--- frontend/src/client/queries.tsx
+++ frontend/src/client/queries.tsx
@@ -1,172 +1,67 @@
-import axios from 'axios';
-
 import { Patient } from './interfaces';
 import { Device } from './interfaces';
 import { Workflow } from './interfaces';
 import { Exam } from './interfaces';
 import { Procedure } from './interfaces';
+import { Job } from './interfaces';
 import { Record } from './interfaces';
 
 import baseUrls from './urls';
 
-// const patientApi = axios.create({baseURL: baseUrls.patientService})
-// const deviceApi = axios.create({baseURL: baseUrls.deviceService})
-// const workflowApi = axios.create({baseURL: baseUrls.workflowService})
-// const examApi = axios.create({baseURL: baseUrls.examService, headers: {
-//     "Content-type": "application/json"
-// }})
+import { ApiService } from './abstract-query-client';
 
-// Patient query client
-const patients = {
 
-    create: async (data: Patient): Promise<Patient> => {
-        const response = await axios.post<Patient>(`${baseUrls.patientService}/`, data);
-        // const response = await patientApi.post('/', data);
-        return response.data;
-    },
-    getAll: async(): Promise<Patient[]> => {
-        const response = await axios.get<Patient[]>(`${baseUrls.patientService}/patients`);
-        // const response = await axios.get(`/patients`);
-        return response.data;
-    },
-    get: async (id: number): Promise<Patient> => {
-        const response = await axios.get<Patient>(`${baseUrls.patientService}/${id}/`);
-        // const response = await patientApi.get(`/${id}`);
-        return response.data;
-    }, 
-    update: async (id: number, data: Partial<Patient>): Promise<Patient> => {
-        const response = await axios.patch<Patient>(`${baseUrls.patientService}/${id}/`, data);
-        // const response = await patientApi.patch(`/${id}`, data);
-        return response.data;
-    },
-    delete: async (id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.patientService}/${id}/`);
-        // await patientApi.delete(`/${id}`);
-    },
+export class PatientApiService extends ApiService<Patient> {
+    constructor() {
+      super(baseUrls.patientService);
+    }
 }
 
-// Device queries
-const devices = {
-    create: async (data: Device): Promise<Device> => {
-        const response = await axios.post<Device>(`${baseUrls.deviceService}/`, data);
-        return response.data;
-    },
-    getAll: async(): Promise<Device[]> => {
-        const response = await axios.get<Device[]>(`${baseUrls.deviceService}/devices/`);
-        return response.data;
-    },
-    get: async(id: number): Promise<Device> => {
-        const response = await axios.get<Device>(`${baseUrls.deviceService}/${id}/`);
-        return response.data;
-    },
-    update: async(id: number, data: Partial<Device>): Promise<Device> => {
-        const response = await axios.patch<Device>(`${baseUrls.deviceService}/${id}/`, data);
-        return response.data;
-    },
-    delete: async(id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.deviceService}/${id}/`);
-    },
+export class DeviceApiService extends ApiService<Device> {
+    constructor() {
+      super(baseUrls.deviceService);
+    }
 }
 
-// Workflow queries
-const workflows = {
-    create: async (data: Workflow): Promise<Workflow> => {
-        const response = await axios.post<Workflow>(`${baseUrls.workflowService}/`, data);
-        return response.data;
-    },
-    getAll: async(): Promise<Workflow[]> => {
-        const response = await axios.get<Workflow[]>(`${baseUrls.workflowService}/workflows/`);
-        return response.data;
-    },
-    get: async(id: number): Promise<Workflow> => {
-        const response = await axios.get<Workflow>(`${baseUrls.workflowService}/${id}/`);
-        return response.data;
-    },
-    update: async(id: number, data: Partial<Workflow>): Promise<Workflow> => {
-        const response = await axios.patch<Workflow>(`${baseUrls.workflowService}/${id}/`, data);
-        return response.data;
-    },
-    delete: async(id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.workflowService}/${id}/`);
-    },
+export class WorkflowApiService extends ApiService<Workflow> {
+    constructor() {
+      super(baseUrls.workflowService);
+    }
 }
 
-// Record queries
-const records = {
-    create: async (data: Record): Promise<Record> => {
-        const response = await axios.post<Record>(`${baseUrls.examService}/record/`, data);
-        return response.data;
-    },
-    getAll: async(): Promise<Record[]> => {
-        const response = await axios.get<Record[]>(`${baseUrls.examService}/records/`);
-        return response.data;
-    },
-    get: async(id: number): Promise<Record> => {
-        const response = await axios.get<Record>(`${baseUrls.examService}/record/${id}/`);
-        return response.data;
-    },
-    update: async(id: number, data: Partial<Record>): Promise<Record> => {
-        const response = await axios.patch<Record>(`${baseUrls.examService}/record/${id}/`, data);
-        return response.data;
-    },
-    delete: async(id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.examService}/record/${id}/`);
-    },
+export class ExamApiService extends ApiService<Exam> {
+    constructor() {
+      super(baseUrls.examService);
+    }
 }
 
-// Procedure queries
-const procedures = {
-    create: async (data: Procedure): Promise<Procedure> => {
-        const response = await axios.post<Procedure>(`${baseUrls.examService}/procedure/`, data);
-        return response.data;
-    },
-    getAll: async(exam_id: number): Promise<Procedure[]> => {
-        const response = await axios.get<Procedure[]>(`${baseUrls.examService}/procedures/${exam_id}/`);
-        return response.data;
-    },
-    get: async(id: number): Promise<Procedure> => {
-        const response = await axios.get<Procedure>(`${baseUrls.examService}/procedure/${id}/`);
-        return response.data;
-    },
-    update: async(id: number, data: Partial<Procedure>): Promise<Procedure> => {
-        const response = await axios.patch<Procedure>(`${baseUrls.examService}/procedure/${id}/`, data);
-        return response.data;
-    },
-    delete: async(id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.examService}/procedure/${id}/`);
-    },
+export class ProcedureApiService extends ApiService<Procedure> {
+    constructor() {
+      super(baseUrls.procedureService);
+    }
 }
-
-// Exam queries
-const exams = {
-    create: async (data: Exam): Promise<Exam> => {
-        const response = await axios.post<Exam>(`${baseUrls.examService}/exam/`, data);
-        return response.data;
-    },
-    getAll: async(patientId: number): Promise<Exam[]> => {
-        const response = await axios.get<Exam[]>(`${baseUrls.examService}/exams/${patientId}/`);
-        // const response = await examApi.get<Exam[]>(`/exams/${patientId}`)
-        return response.data;
-    },
-    get: async(id: number): Promise<Exam> => {
-        const response = await axios.get<Exam>(`${baseUrls.examService}/procedure/${id}/`);
-        return response.data;
-    },
-    update: async(id: number, data: Partial<Exam>): Promise<Exam> => {
-        const response = await axios.patch<Exam>(`${baseUrls.examService}/procedure/${id}/`, data);
-        return response.data;
-    },
-    delete: async(id: number): Promise<void> => {
-        await axios.delete(`${baseUrls.examService}/procedure/${id}/`);
-    },
+  
+export class JobApiService extends ApiService<Job> {
+    constructor() {
+      super(baseUrls.jobService);
+    }
 }
 
 
+const patientService = new PatientApiService();
+const workflowService = new WorkflowApiService();
+const deviceService = new DeviceApiService();
+const examService = new ExamApiService();
+const procedureService = new ProcedureApiService();
+const jobService = new JobApiService();
+
+
+// Export all services in one client
 export default {
-    patients,
-    devices,
-    workflows,
-    records,
-    procedures,
-    exams,
+    patientService,
+    workflowService,
+    deviceService,
+    examService,
+    procedureService,
+    jobService,
 }
