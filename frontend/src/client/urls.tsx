c9a76a3f805fe660ff9348688cf1a915defe5a30
David Schote <david.schote@web.de>
2023-05-03 22:27:48 +0200

    Update device url

--- frontend/src/client/urls.tsx
+++ frontend/src/client/urls.tsx
@@ -1,8 +1,8 @@
 
 const baseUrls = {
     patientService: 'http://localhost:8100',
-    deviceService: ' http://localhost:8002/api/v1/devices',
-    workflowService: 'http://localhost:8080/api/v1/workflows',
+    deviceService: ' http://localhost:8002/api/v1/device',
+    workflowService: 'http://localhost:8080/api/v1/workflow',
     examService: 'http://localhost:8080/api/v1/exam',
     procedureService: 'http://localhost:8080/api/v1/exam/procedure',
     jobService: 'http://localhost:8080/api/v1/exam/job',
