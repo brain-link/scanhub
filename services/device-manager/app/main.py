4ba4d9bc4cb1cac9988425ffcced26760d2966e2
David Schote <david.schote@web.de>
2023-05-21 22:25:31 +0200

    Removed import

--- services/device-manager/app/main.py
+++ services/device-manager/app/main.py
@@ -5,7 +5,7 @@ from api.devices import router
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import inspect
-from starlette.responses import FileResponse
+
 
 app = FastAPI(
     openapi_url="/api/v1/device/openapi.json",
