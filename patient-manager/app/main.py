87fa0606cfde827fe74b82b21857c02bc2642bc5
David Schote <david.schote@web.de>
2023-05-29 22:23:19 +0200

    Fix dependencies

--- patient-manager/app/main.py
+++ patient-manager/app/main.py
@@ -1,10 +1,9 @@
 """Patient manager main file."""
 
+import api.db as db
+import api.routes as routes
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from .api.db import init_db
-from .api.routes import router
 
 app = FastAPI(
     title="ScanHub-UI"
@@ -23,7 +22,7 @@ app.add_middleware(
 @app.on_event("startup")
 async def startup() -> None:
     """Initialize patient database."""
-    init_db()
+    db.init_db()
 
 
-app.include_router(router)
+app.include_router(routes.router)
