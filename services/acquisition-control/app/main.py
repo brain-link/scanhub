2a078af8df526863cd29ee5f8f1df4873e7c57dd
Christoph Dinh <christoph.dinh@brain-link.de>
2023-06-05 14:04:43 -0400

    added debug flag

--- services/acquisition-control/app/main.py
+++ services/acquisition-control/app/main.py
@@ -8,12 +8,14 @@ import logging
 import json
 import requests
 
+import random
+
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Extra, Field  # pylint: disable=no-name-in-module
 
 #DEVICE_URI = "host.docker.internal:8001"
-DEVICE_URI = "127.0.0.1:5000"
+DEVICE_URI = "host.docker.internal:5000"
 SEQUENCE_MANAGER_URI = "host.docker.internal:8003"
 EXAM_MANAGER_URI = "host.docker.internal:8004"
 
@@ -59,35 +61,54 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.post("/api/v1/mri/acquisitioncontrol/start-scan")
 async def start_scan(scan_job: ScanJob):
+    """Receives a job. Create a record id, trigger scan with it and returns it"""
     # TODO: Dont ignore device_id, check returns, ... 
 
-    """Receives a job. Create a record id, trigger scan with it and returns it"""
-    # get sequence
-    res = requests.get(
-        f"http://{SEQUENCE_MANAGER_URI}/api/v1/mri/sequences/{scan_job.sequence_id}", timeout=60)
-    sequence_json = res.json()
+    debug_flag = True
+    if debug_flag:
+        record_id = "test_"+str(random.randint(0,1000))
+        sequence_json = {"test": "test"}
 
-    # create record
-    # TODO: data_path, comment ?
-    res = requests.post(f"http://{EXAM_MANAGER_URI}/api/v1/exam/record",
-                        json={"data_path": "path",
-                              "comment": "blub",
-                              "job_id": scan_job.job_id}, timeout=60)
-    record_id = res.json()["id"]
+        url = f"http://{DEVICE_URI}/api/start-scan"
+        print(url)
+        response = requests.post(url,
+                            json={"record_id": record_id,
+                                "sequence": json.dumps(sequence_json)}, timeout=60)
+        
+        if response.status_code == 200:
+            print('Scan started successfully.')
+        else:
+            print('Failed to start scan.')
 
-    # start scan and forward sequence, workflow, record_id
-    logging.debug("Received job: %s, Generated record id: %s", scan_job.job_id, record_id)
-    print(sequence_json)
-
-    url = f"http://{DEVICE_URI}/api/start-scan"
-    response = requests.post(url,
-                        json={"record_id": record_id,
-                              "sequence": json.dumps(sequence_json)}, timeout=60)
-    
-    if response.status_code == 200:
-        print('Scan started successfully.')
     else:
-        print('Failed to start scan.')
+        """Receives a job. Create a record id, trigger scan with it and returns it"""
+        # get sequence
+        res = requests.get(
+            f"http://{SEQUENCE_MANAGER_URI}/api/v1/mri/sequences/{scan_job.sequence_id}", timeout=60)
+        sequence_json = res.json()
+
+        # create record
+        # TODO: data_path, comment ?
+        res = requests.post(f"http://{EXAM_MANAGER_URI}/api/v1/exam/record",
+                            json={"data_path": "path",
+                                "comment": "blub",
+                                "job_id": scan_job.job_id}, timeout=60)
+        record_id = res.json()["id"]
+
+        # start scan and forward sequence, workflow, record_id
+        logging.debug("Received job: %s, Generated record id: %s", scan_job.job_id, record_id)
+        print(sequence_json)
+
+        url = f"http://{DEVICE_URI}/api/start-scan"
+        print(url)
+        response = requests.post(url,
+                            json={"record_id": record_id,
+                                "sequence": json.dumps(sequence_json)}, timeout=60)
+        
+        if response.status_code == 200:
+            print('Scan started successfully.')
+        else:
+            print('Failed to start scan.')
 
     print(response)
     return {"record_id": record_id}
