a7e074f8a66fa0f8f67d33b2c3ead137abfddd38
Christoph Dinh <christoph.dinh@brain-link.de>
2023-06-21 22:13:42 +0200

    skip flake 8 for __init__.py

--- services/mri/sequence-manager/app/endpoints/__init__.py
+++ services/mri/sequence-manager/app/endpoints/__init__.py
@@ -1,2 +1,2 @@
-from .health import router as health_router
-from .mri_sequence_endpoints import router as mri_sequence_router
+from .health import router as health_router  # noqa : F401
+from .mri_sequence_endpoints import router as mri_sequence_router  # noqa : F401
--- services/mri/sequence-manager/app/services/__init__.py
+++ services/mri/sequence-manager/app/services/__init__.py
@@ -1,9 +1,7 @@
-from .mri_sequence_service import (
-    create_mri_sequence,
-    delete_mri_sequence,
-    download_mri_sequence_file,
-    get_mri_sequence_by_id,
-    get_mri_sequences,
-    search_mri_sequences,
-    update_mri_sequence,
-)
+from .mri_sequence_service import create_mri_sequence  # noqa : F401
+from .mri_sequence_service import delete_mri_sequence  # noqa : F401
+from .mri_sequence_service import download_mri_sequence_file  # noqa : F401
+from .mri_sequence_service import get_mri_sequence_by_id  # noqa : F401
+from .mri_sequence_service import get_mri_sequences  # noqa : F401
+from .mri_sequence_service import search_mri_sequences  # noqa : F401
+from .mri_sequence_service import update_mri_sequence  # noqa : F401
