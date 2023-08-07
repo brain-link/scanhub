29c3cf3a4ab433b24ae236c1754d5644d96b5785
Christoph Dinh <christoph.dinh@brain-link.de>
2023-08-07 23:47:45 +0200

    added exam openapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.openapi'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
