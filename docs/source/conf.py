bc242a2a923111dbf6ad34bc70a174a7f8055ab3
Christoph Dinh <christoph.dinh@brain-link.de>
2023-08-21 14:41:51 +0200

    switch theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata-sphinx-theme'
 html_show_sphinx = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
