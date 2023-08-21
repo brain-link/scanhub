ba5032a72e8de077d095cfedb34e565774f66106
Christoph Dinh <christoph.dinh@brain-link.de>
2023-08-21 14:46:43 +0200

    add pydata theme to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinxcontrib.openapi'
+    'sphinxcontrib.openapi',
+    'pydata-sphinx-theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
