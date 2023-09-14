571c3793e8bc2f90bb3c76a243bb5863c590b33c
David Schote <david.schote@web.de>
2023-09-14 18:45:04 +0200

    Device component

--- frontend/src/interfaces/components.interface.tsx
+++ frontend/src/interfaces/components.interface.tsx
@@ -40,3 +40,21 @@ export interface JobModalProps extends Omit<JobComponentProps, "data"> {
 export interface SequenceViewerProps {
     sequence_id: string;
 }
+
+
+export enum Alerts {
+    Success = "success",
+    Warning = "warning",
+    Danger = "danger",
+    Neutral = "neutral"
+}
+
+export interface AlertProps {
+    title: string;
+    info?: string;
+    type: Alerts;
+}
+
+export interface ExamTreeProps {
+    setDataPath: (dataPath: string) => void;
+}
