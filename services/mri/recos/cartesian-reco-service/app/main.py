6c1a9a7500c8d9d9a4168635ff26db605df3914c
Christoph Dinh <christoph.dinh@brain-link.de>
2023-09-19 16:19:20 +0200

    reco linted

--- services/mri/recos/cartesian-reco-service/app/main.py
+++ services/mri/recos/cartesian-reco-service/app/main.py
@@ -14,8 +14,7 @@ from aiokafka import AIOKafkaConsumer
 from api.worker import init, run
 from fastapi import FastAPI
 from kafka import TopicPartition
-from kafka.errors import NoBrokersAvailable, KafkaConnectionError, GroupCoordinatorNotAvailableError
-import time
+from kafka.errors import GroupCoordinatorNotAvailableError, KafkaConnectionError, NoBrokersAvailable
 
 # instantiate the API
 app = FastAPI()
@@ -33,6 +32,7 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
 @app.on_event("startup")
 async def startup_event():
     """Startup event for the API."""
@@ -40,27 +40,30 @@ async def startup_event():
     await initialize()
     await consume()
 
+
 @app.on_event("shutdown")
 async def shutdown_event():
-    """Shutdown event for the API."""    
+    """Shutdown event for the API."""
     log.info("Shutting down API")
     CONSUMER_TASK.cancel()
     await CONSUMER.stop()
+
 
 @app.get("/")
 async def root():
     """Root endpoint for the API."""
     return {"message": "Cartesian Reco Service"}
 
+
 async def initialize():
     """Initialize the API."""
     loop = asyncio.get_event_loop()
-    global CONSUMER
+    global CONSUMER # pylint: disable=global-statement
     group_id = f"{KAFKA_CONSUMER_GROUP_PREFIX}-{randint(0, 10000)}"
     log.debug(
         f"Initializing KafkaConsumer for topic {KAFKA_TOPIC}, group_id "
         f"{group_id} and using bootstrap servers {KAFKA_BOOTSTRAP_SERVERS}"
-    )
+    ) # pylint: disable=logging-fstring-interpolation
 
     retries = 5
     for i in range(retries):
@@ -102,7 +105,7 @@ async def initialize():
             # pylint: disable=logging-fstring-interpolation
             log.warning(
                 f"Topic {KAFKA_TOPIC} has no messages (log_end_offset: " f"{end_offset}), skipping initialization ..."
-            )
+            ) # pylint: disable=logging-fstring-interpolation
             return
 
         log.debug(f"Found log_end_offset: {end_offset} seeking to {end_offset - 1}")
@@ -114,10 +117,12 @@ async def initialize():
         init(msg)
         return
 
+
 async def consume():
     """Consume messages from the Kafka topic."""
-    global CONSUMER_TASK
+    global CONSUMER_TASK # pylint: disable=global-statement
     CONSUMER_TASK = asyncio.create_task(send_consumer_message(CONSUMER))
+
 
 async def send_consumer_message(consumer):
     """Send consumer message to the worker.
