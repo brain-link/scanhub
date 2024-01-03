a52e73d5ac3b3a05d3a84fe106cc34ec67715128
David Schote <david.schote@web.de>
2024-01-03 12:19:40 +0100

    Added models to base image

--- base/shared_libs/setup.py
+++ base/shared_libs/setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
-setup(name='scanhub_models',
-      version='1.0',
-      py_modules=['scanhub_models'],
-      package_data={"scanhub_models": ["py.typed"]}
-      )
+
+setup(
+    name="scanhub_models",
+    version="1.0",
+    py_modules=["scanhub_models"],
+    package_data={"scanhub_models": ["py.typed"]},
+)
