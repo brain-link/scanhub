4af3dd21d3e396fa28275f28ce29a2ef185ad44d
Johannes Behrens <johannes.behrens@posteo.net>
2024-01-10 17:26:42 +0100

    change to package

--- base/shared_libs/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="scanhub_models",
-    version="1.0",
-    py_modules=["scanhub_models"],
-    package_data={"scanhub_models": ["py.typed"]},
-)
--- base/shared_libs/src/scanhub_libraries/models.py
+++ base/shared_libs/src/scanhub_libraries/models.py
@@ -95,8 +95,8 @@ class MRISequence(BaseModel):
     name: str
     description: str | None
     sequence_type: str | None
-    created_at: datetime.datetime | None
-    updated_at: datetime.datetime | None
+    created_at: datetime | None
+    updated_at: datetime | None
     tags: list[str] | None
     file: Any
     file_extension: str | None
@@ -120,8 +120,8 @@ class MRISequenceCreate(BaseModel):
     name: str
     description: str | None = None
     sequence_type: str | None = None
-    created_at: datetime.datetime | None = None
-    updated_at: datetime.datetime | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
     tags: list[str] | None = None
 
 
