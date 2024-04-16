7d27c7ca08f6ffc70c027b1c8ff4b97c329a00fe
David Schote <david.schote@web.de>
2024-04-17 00:26:14 +0200

    Added workflow and task api client instances

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,6 +1,10 @@
 import baseUrls from './client/urls'
 import { PatientsApi } from "./generated-client/patient"
 import { ExamsApi } from './generated-client/exam'
+import { WorkflowsApi } from './generated-client/exam'
+import { TasksApi } from './generated-client/exam'
 
 export const patientApi = new PatientsApi(undefined, baseUrls.patientService)
 export const examApi = new ExamsApi(undefined, baseUrls.examService)
+export const workflowsApi = new WorkflowsApi(undefined, baseUrls.examService)
+export const taskApi = new TasksApi(undefined, baseUrls.examService)
