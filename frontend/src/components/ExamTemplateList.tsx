3d501815bca88c5ce08274a45b99e24874bff4d9
David Schote <david.schote@web.de>
2024-04-17 00:29:13 +0200

    Updated exam template list component

--- frontend/src/components/ExamTemplateList.tsx
+++ frontend/src/components/ExamTemplateList.tsx
@@ -1,19 +1,26 @@
 // Copyright (C) 2023, BRAIN-LINK UG (haftungsbeschränkt). All Rights Reserved.
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-ScanHub-Commercial
 // PatientTable.tsx is responsible for rendering the patient table view.
-import Stack from '@mui/joy/Stack'
 import * as React from 'react'
+import Stack from '@mui/joy/Stack'
+import Add from '@mui/icons-material/Add'
+import Button from '@mui/joy/Button'
+
+import ExamTemplateItem from '../components/ExamTemplateItem'
+import AlertItem from '../components/AlertItem'
+import ExamTemplateCreateModal from '../components/ExamTemplateCreateModal'
+
+import { Alerts } from '../interfaces/components.interface'
 import { useQuery } from 'react-query'
 import { ExamOut } from "../generated-client/exam";
 import { examApi } from '../Api'
-import AlertItem from '../components/AlertItem'
-import { Alerts } from '../interfaces/components.interface'
-import ExamTemplateItem from '../components/ExamTemplateItem'
 
 
 export default function ExamTemplateList() {
 
-  const {data: exams, isLoading, isError} = useQuery<ExamOut[]>({
+  const [modalOpen, setModalOpen] = React.useState(false)
+
+  const {data: exams, isLoading, isError, refetch} = useQuery<ExamOut[]>({
     queryKey: ['exams'],
     queryFn: async () => { return await examApi.getAllExamTemplatesApiV1ExamTemplatesAllGet().then((result) => {return result.data})}
   })
@@ -24,6 +31,23 @@ export default function ExamTemplateList() {
       alignItems="flex-start"
       spacing={2}
     >
+      <Button
+        startDecorator={<Add />}
+        onClick={() => setModalOpen(true)}
+      >
+        Create Exam Template
+      </Button>
+
+      <ExamTemplateCreateModal
+        isOpen={modalOpen}
+        setOpen={setModalOpen}
+        onSubmit={
+          // (newExam: ExamOut) => { exams?.push(newExam) }
+          () => { refetch() }
+        }
+        onClose={() => {}}
+      />
+
       {
         exams?.map((exam) => (
           <ExamTemplateItem
