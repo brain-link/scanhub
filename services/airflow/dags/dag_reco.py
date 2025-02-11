c5edb92cb1699b4164e6e6aa3f9ac405d584e99b
Christoph Dinh <christoph.dinh@brain-link.de>
2025-02-11 01:26:32 +0100

    introduce own dag_reco

--- services/airflow/dags/dag_reco.py
+++ services/airflow/dags/dag_reco.py
@@ -1,7 +1,19 @@
+# [START import_module]
+import os
+import json
+import numpy as np
+import pydicom
+import pydicom._storage_sopclass_uids
+
+
 from datetime import datetime, timedelta
 
 from airflow.decorators import dag, task
+from airflow.models import Variable
 
+from pydantic import BaseModel, StrictStr
+from pydicom.dataset import Dataset
+# [END import_module]
 
 default_args = {
     'owner': 'BRAIN-LINK',
@@ -9,32 +21,91 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+# Attempting to use mkl_fft (faster FFT library for Intel CPUs). Fallback is np
+try:
+    import mkl_fft as m  # type: ignore
+
+    fft2 = m.fft2
+    ifft2 = m.ifft2
+except (ModuleNotFoundError, ImportError):
+    fft2 = np.fft.fft2
+    ifft2 = np.fft.ifft2
+finally:
+    fftshift = np.fft.fftshift
+    ifftshift = np.fft.ifftshift
+
+# [START instantiate_dag]
 @dag(dag_id='dag_reco_v01', 
      default_args=default_args, 
      start_date=datetime(2021, 10, 26), 
      schedule_interval='@daily')
-def hello_world_etl():
+def cartesian_reco_etl():
+    """
+    ### TaskFlow API Tutorial Documentation
+    This is a simple ETL data pipeline example which demonstrates the use of
+    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
+    Documentation that goes along with the Airflow TaskFlow API tutorial is
+    located
+    [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
+    """
+    # [END instantiate_dag]
 
+    # [START extract]
+    @task()
+    def extract():
+        """
+        #### Extract task
+        A simple Extract task to get data ready for the rest of the data
+        pipeline. In this case, getting data is simulated by reading from a
+        hardcoded JSON string.
+        """
+        # Get env just for demo purposes
+        env = Variable.get("env")
+        print(f"Running in {env} environment")
+
+        # Generate random data
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
+
+    # [END extract]
+
+    # [START transform]
     @task(multiple_outputs=True)
-    def get_name():
-        return {
-            'first_name': 'Jerry',
-            'last_name': 'Fridman'
-        }
+    def transform(order_data_dict: dict):
+        """
+        #### Transform task
+        A simple Transform task which takes in the collection of order data and
+        computes the total order value.
+        """
+        total_order_value = 0
 
+        for value in order_data_dict.values():
+            total_order_value += value
+
+        return {"total_order_value": total_order_value}
+
+    # [END transform]
+
+    # [START load]
     @task()
-    def get_age():
-        return 19
+    def load(total_order_value: float):
+        """
+        #### Load task
+        A simple Load task which takes in the result of the Transform task and
+        instead of saving it to end user review, just prints it out.
+        """
 
-    @task()
-    def greet(first_name, last_name, age):
-        print(f"Hello World! My name is {first_name} {last_name} "
-              f"and I am {age} years old!")
-    
-    name_dict = get_name()
-    age = get_age()
-    greet(first_name=name_dict['first_name'], 
-          last_name=name_dict['last_name'],
-          age=age)
+        print(f"Total order value is: {total_order_value:.2f}")
 
-greet_dag = hello_world_etl()
+    # [END load]
+
+    # [START main_flow]
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+    # [END main_flow]
+
+# [START dag_invocation]
+reco_dag = cartesian_reco_etl()
+# [END dag_invocation]
