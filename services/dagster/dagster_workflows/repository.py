c85ae477586f2ceb4a9ae22c827912af69cf73f4
David Schote <david.schote@web.de>
2025-08-22 15:20:28 +0200

    Updated dagster configuration

--- services/dagster/dagster_workflows/repository.py
+++ services/dagster/dagster_workflows/repository.py
@@ -1,21 +1,13 @@
 """Define dagster repository."""
-from dagster import repository
+from dagster import Definitions, in_process_executor
 
 from dagster_workflows.frequency_calibration import frequency_calibration_job
 from dagster_workflows.mrpro_image_reconstruction import mrpro_reconstruction_job
 
-
-@repository
-def reconstruction_repository() -> list:
-    """Define the reconstruction repository."""
-    return [
+definitions = Definitions(
+    jobs=[
         mrpro_reconstruction_job,
-    ]
-
-
-@repository
-def processing_repository() -> list:
-    """Define the processing repository."""
-    return [
         frequency_calibration_job,
-    ]
+    ],
+    executor=in_process_executor,   # default executor for all jobs
+)
