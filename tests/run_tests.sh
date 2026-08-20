a9d6d41f752968d7bb60ca4b710da64c5a0f6e6a
David Schote <david.schote@web.de>
2026-08-20 18:14:34 +0200

    Removed outdated tests

--- tests/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to run all tests.
-
-cd services/protocol-manager
-source .env/bin/activate
-pytest
-echo "Please consider that failed tests might in some cases leave database entries behind."
-deactivate
